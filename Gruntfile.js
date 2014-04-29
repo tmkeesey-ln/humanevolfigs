0172e13e55584a854160dbd31e59a71f017d81e6
T. Michael Keesey <keesey@gmail.com>
2014-04-29 01:17:37 -0700

    Added an `analyze` task, with one target (`synapomorphies`). Started reviewing Strait & Grine 2004 and recoding "Variable" states as polymorphisms in the `Table3-modified` matrix. (Will need to add functionality to discern character type on a character-by-character basis.)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,139 +2,175 @@
 
 module.exports = function(grunt)
 {
-  var TYPESCRIPT_OPTIONS =
-  {
-    target: 'es5',
-    base_path: 'src',
-    declaration: false,
-    sourcemap: false,
-    noImplicitAny: true
-  };
+	var TYPESCRIPT_OPTIONS =
+	{
+		target: 'es5',
+		base_path: 'src',
+		declaration: false,
+		sourcemap: false,
+		noImplicitAny: true
+	};
 
-  // Show elapsed time at the end
-  require('time-grunt')(grunt);
+	// Show elapsed time at the end
+	require('time-grunt')(grunt);
 
-  // Load all grunt tasks
-  require('load-grunt-tasks')(grunt);
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-typescript');
-  grunt.loadNpmTasks('grunt-mkdir');
+	// Load all grunt tasks
+	require('load-grunt-tasks')(grunt);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-mkdir');
 
-  grunt.registerMultiTask('render', 'Renders SVG and PNG files for a figure.', function()
-  {
-    var done = this.async(),
-      name = this.target,
-      msg = 'Rendering "' + name + '"...';
-    grunt.verbose.write(msg);
-    grunt.util.spawn({
-      cmd: 'phantomjs',
-      args: ['bower_components/haeckel/bin/render.js', 'src/' + name + '.fig.js', 'bin/']
-    }, function (error, result, code)
+	grunt.registerMultiTask('render', 'Renders SVG and PNG files for a figure.', function()
+	{
+		var done = this.async(),
+			name = this.target,
+			msg = 'Rendering "' + name + '"...';
+		grunt.verbose.write(msg);
+		grunt.util.spawn({
+			cmd: 'phantomjs',
+			args: ['bower_components/haeckel/bin/render.js', 'src/' + name + '.fig.js', 'bin/']
+		}, function (error, result, code)
+		{
+			if (error)
+			{
+				grunt.verbose.or.write(msg).error().error('Error #' + code);
+				grunt.fail.warn('Error rendering "' + name + '".');
+				done(false);
+			}
+			else
+			{
+				grunt.verbose.ok();
+				done(true);
+			}
+		});
+	});
+
+	grunt.registerMultiTask('figure', 'Compiles and renders a figure', function()
+	{
+		grunt.task.run('typescript:' + this.target);
+		grunt.task.run('render:' + this.target);
+	});
+
+    grunt.registerMultiTask('analyze', 'Runs an analysis', function()
     {
-      if (error)
-      {
-        grunt.verbose.or.write(msg).error().error('Error #' + code);
-        grunt.fail.warn('Error rendering "' + name + '".');
-        done(false);
-      }
-      else
-      {
-        grunt.verbose.ok();
-        done(true);
-      }
-    });
-  });
-
-  grunt.registerMultiTask('figure', 'Compiles and renders a figure', function()
-  {
-    grunt.task.run('typescript:' + this.target);
-    grunt.task.run('render:' + this.target);
-  });
-
-  // Project configuration.
-  grunt.initConfig(
-  {
-    clean:
-    {
-      bin: ['bin/'],
-      lib: ['src/*.js']
-    },
-    typescript:
-    {
-      craniodental:
-      {
-        src: [ 'src/distanceChart.ts',  'src/craniodental.fig.ts' ],
-        dest: 'src/craniodental.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      },
-      craniodentalExtant:
-      {
-        src: [ 'src/distanceChart.ts',  'src/craniodentalExtant.fig.ts' ],
-        dest: 'src/craniodentalExtant.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      },
-      extant:
-      {
-        src: [ 'src/distanceChart.ts',  'src/extant.fig.ts' ],
-        dest: 'src/extant.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      },
-      geoChron:
-      {
-        src: [ 'src/geoChron.fig.ts' ],
-        dest: 'src/geoChron.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      },
-      mtDNA:
-      {
-        src: [ 'src/haploMap.ts', 'src/mtDNA.fig.ts' ],
-        dest: 'src/mtDNA.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      },
-      softTissue:
-      {
-        src: [ 'src/distanceChart.ts',  'src/softTissue.fig.ts' ],
-        dest: 'src/softTissue.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      },
-      YDNA:
-      {
-        src: [ 'src/haploMap.ts', 'src/YDNA.fig.ts' ],
-        dest: 'src/YDNA.fig.js',
-        options: TYPESCRIPT_OPTIONS
-      }
-    },
-    mkdir:
-    {
-      bin: {
-        options:
+        var done = this.async(),
+            name = this.target,
+            msg = 'Analyzing "' + name + '"...';
+        grunt.verbose.write(msg);
+        grunt.task.run('typescript:' + name);
+        grunt.util.spawn({
+            cmd: 'cat',
+            args: ['bower_components/haeckel/bin/haeckel.js', 'src/' + name + '.sh.js', '|', 'node', '>', 'bin/' + name + '.json']
+        }, function (error, result, code)
         {
-          create: ['bin']
-        }
-      }
-    },
-    render:
-    {
-      craniodental: {},
-      craniodentalExtant: {},
-      extant: {},
-      geoChron: {},
-      mtDNA: {},
-      softTissue: {},
-      YDNA: {}
-    },
-    figure:
-    {
-      craniodental: {},
-      craniodentalExtant: {},
-      extant: {},
-      geoChron: {},
-      mtDNA: {},
-      softTissue: {},
-      YDNA: {}
-    }
-    // :TODO: watch tasks
-  });
+            if (error)
+            {
+                grunt.verbose.or.write(msg).error().error('Error #' + code);
+                grunt.fail.warn('Error analyzing "' + name + '".');
+                done(false);
+            }
+            else
+            {
+                grunt.verbose.ok();
+                done(true);
+            }
+        });
+    });
 
-  grunt.registerTask('default', ['clean', 'mkdir', 'figure']);
+	// Project configuration.
+	grunt.initConfig(
+	{
+		clean:
+		{
+			bin: ['bin/'],
+			lib: ['src/*.js']
+		},
+		typescript:
+		{
+			craniodental:
+			{
+				src: [ 'src/distanceChart.ts',	'src/craniodental.fig.ts' ],
+				dest: 'src/craniodental.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			craniodentalExtant:
+			{
+				src: [ 'src/distanceChart.ts',	'src/craniodentalExtant.fig.ts' ],
+				dest: 'src/craniodentalExtant.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			extant:
+			{
+				src: [ 'src/distanceChart.ts',	'src/extant.fig.ts' ],
+				dest: 'src/extant.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			geoChron:
+			{
+				src: [ 'src/geoChron.fig.ts' ],
+				dest: 'src/geoChron.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			mtDNA:
+			{
+				src: [ 'src/haploMap.ts', 'src/mtDNA.fig.ts' ],
+				dest: 'src/mtDNA.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			softTissue:
+			{
+				src: [ 'src/distanceChart.ts',	'src/softTissue.fig.ts' ],
+				dest: 'src/softTissue.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			synapomorphies:
+			{
+				src: [ 'src/synapomorphies.sh.ts' ],
+				dest: 'src/synapomorphies.sh.js',
+				options: TYPESCRIPT_OPTIONS
+			},
+			YDNA:
+			{
+				src: [ 'src/haploMap.ts', 'src/YDNA.fig.ts' ],
+				dest: 'src/YDNA.fig.js',
+				options: TYPESCRIPT_OPTIONS
+			}
+		},
+		mkdir:
+		{
+			bin: {
+				options:
+				{
+					create: ['bin']
+				}
+			}
+		},
+		render:
+		{
+			craniodental: {},
+			craniodentalExtant: {},
+			extant: {},
+			geoChron: {},
+			mtDNA: {},
+			softTissue: {},
+			YDNA: {}
+		},
+		figure:
+		{
+			craniodental: {},
+			craniodentalExtant: {},
+			extant: {},
+			geoChron: {},
+			mtDNA: {},
+			softTissue: {},
+			YDNA: {}
+		},
+        analyze:
+        {
+            synapomorphies: {}
+        }
+		// :TODO: watch tasks
+	});
+
+	grunt.registerTask('default', ['clean', 'mkdir', 'figure', 'analyze']);
 };
---
+++ src/synapomorphies.sh.ts
@@ -0,0 +1,71 @@
+/// <reference path="../bower_components/haeckel/bin/haeckel.d.ts"/>
+/// <reference path="../bower_components/dt-node/node.d.ts"/>
+
+var NAMES: string[] = ['Pongo pygmaeus', 'Gorilla gorilla', 'Pan troglodytes', 'Homo sapiens'];
+
+var NAMES2: string[] = ['Hominidae', 'Homininae', 'Hominini', 'Homo sapiens'];
+
+var SOURCES: string[] = [ 'compiled/nomenclature.json', '2004 - Strait & Grine.json', '2011 - Diogo & Wood.json' ];
+
+var fs = require('fs');
+
+var files: Haeckel.FileCache = {
+		base64: {},
+		text: {}
+	};
+
+for (var i = 0, n = SOURCES.length; i < n; ++i)
+{
+	var filename = SOURCES[i];
+	files.text[filename] = fs.readFileSync('./src/data/' + filename, "utf8");
+}
+
+var dataSources = new Haeckel.DataSourcesReader().read(files, SOURCES);
+
+var taxa: Haeckel.Taxic[] = [];
+
+for (i = 0, n = NAMES.length; i < n; ++i)
+{
+	var subtaxa: Haeckel.Taxic[] = [];
+	for (var j = i; j < n; ++j)
+	{
+		subtaxa.push(dataSources.nomenclature.nameMap[NAMES[j]]);
+	}
+	taxa.push(Haeckel.tax.union(subtaxa));
+}
+
+var output: any = {};
+
+for (filename in dataSources.sources)
+{
+	var source = dataSources.sources[filename];
+	var sourceOutput: any = output[filename] = {};
+	for (var matrixName in source.characterMatrices)
+	{
+		var matrix = source.characterMatrices[matrixName];
+		var matrixOutput: any = sourceOutput[matrixName] = {};
+		var m = matrix.characterList.length;
+		for (i = 0, n = taxa.length; i < n; ++i)
+		{
+			var taxon = taxa[i];
+			var taxonName = NAMES2[i];
+			var taxonOutput: any = matrixOutput[taxonName] = {};
+			var outgroup = Haeckel.tax.setDiff(matrix.taxon, dataSources.nomenclature.nameMap[taxonName]);
+			for (j = 0; j < m; ++j)
+			{
+				var character = matrix.characterList[j];
+				if (character.distance)
+				{
+					var inStates = Haeckel.chr.states(matrix, taxon, character);
+					var outStates = Haeckel.chr.states(matrix, outgroup, character);
+					if (!character.overlap(inStates, outStates))
+					{
+						taxonOutput[j + 1] = Haeckel.hash(inStates);
+					}
+				}
+			}
+		}
+	}
+}
+
+console.log(JSON.stringify(output, null, '\t'));
